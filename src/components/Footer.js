fe5515d13e8e1cdb95bffdedeecba8274cf8fe5b
om mapari <mapariom05@gmail.com>
2023-03-20 17:19:06 +0530

    header-added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-dark text-white">
+      <div className="container py-3">
+        <div className="row">
+          {
+            Array(3).fill(null).map(() => {
+              return (
+                <div className="col-sm-4 bg-dark">
+                  <h5 style={{ color: '#ffffff' }}>Column 1</h5>
+                  <ul className="list-unstyled" style={{ color: '#999999' }}>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                  </ul>
+                </div>
+              )
+            })
+          }
+        </div>
+        <hr />
+        <div className="text-center" style={{ backgroundColor: '#111111' }}>
+          <p style={{ color: '#999999' }}>&copy; 2023 Example Company. All Rights Reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,79 @@
-// import logo from "../logo.png";
-import { Link } from "react-router-dom"; // component
-import React from "react";
+// import React from 'react';
+// import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import logo from '../assets/logo.jpg';
+import { Link } from 'react-router-dom'; // add this line
 
-function Header() {
+// function Header() {
+//     return (
+//         <Navbar bg="light" expand="md">
+//             <Navbar.Brand href="#home">
+//                 <img
+//                     src={logo}
+//                     height="30"
+//                     className="d-inline-block align-top"
+//                     alt="Logo"
+//                 />
+//             </Navbar.Brand>
+//             <Navbar.Toggle aria-controls="navbar-nav" />
+//             <Navbar.Collapse id="navbar-nav">
+//                 <Nav className="navbar-nav ms-auto mb-2 mb-lg-0">
+//                     <Link to="/" className="nav-link">HOME</Link>
+//                     <Link to="/programs" className="nav-link">OUR PROGRAMS</Link>
+//                     <Link to="/about" className="nav-link">OUR STORY</Link>
+//                     <Link to="/our-brands" className="nav-link">OUR BRANDS</Link>
+//                     <Link to="/initiatives" className="nav-link">SOCIAL INITIATIVES</Link>
+//                     <Link to="/contact-us" className="nav-link">CONTACT US</Link>
+
+//                 </Nav>
+//             </Navbar.Collapse>
+//         </Navbar>
+//     );
+// }
+
+// export default Header;
+
+
+
+
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
     return (
-        <div className="header">
-            {/* <Link to="/">
-                <img className="logo" src={logo} alt="logo" />
-            </Link> */}
+        <Navbar collapseOnSelect expand="lg" bg="white" variant="white">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <Link to="/" className="nav-link">
+                        <img
+                            src={logo}
+                            height="60"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        />
+
+                    </Link>
 
 
-    
-            <div className="nav-items">
-                <ul>
-                    <li>
-                        <Link to="/"> HOME </Link>
-                    </li>
-                    <li>
-                        <Link to="/programs">Our Programs </Link>
-                    </li>
-                    <li>
-                        <Link to="/about">OUR STORY </Link>
-                    </li>
-                    <li>
-                        <Link to="/our-brands">OUR BRANDS</Link>
-                    </li>
-                    <li>
-                        <Link to="/initiatives">SOCIAL INITIATIVES</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact-us">CONTACT US </Link>
-                    </li>
-                </ul>
-            </div>
+                </Navbar.Brand>
 
-        </div>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto"> </Nav>
+                    <Nav>
+                        <Link to="/programs" className="nav-link">OUR PROGRAMS</Link>
+                        <Link to="/about" className="nav-link">OUR STORY</Link>
+                        <Link to="/our-brands" className="nav-link">OUR BRANDS</Link>
+                        <Link to="/initiatives" className="nav-link">SOCIAL INITIATIVES</Link>
+                        <Link to="/contact-us" className="nav-link">CONTACT US</Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
-export default Header;
+export default CollapsibleExample;
