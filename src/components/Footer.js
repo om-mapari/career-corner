9fe0efc80319d31e8e7f4a87d421d63201475c5e
om mapari <mapariom05@gmail.com>
2023-03-21 03:38:37 +0530

    Header and Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 function Footer() {
+  const address = [
+    'Phone', '+91 72766-30705', 'Email', 'admin@merito.in', 'Address', 'Baner,Pune,Maharashtra 411045'
+  ]
+  const our = [
+    'Our Programs','Our Story','Our Story','Social Initiatives','Contact Us'
+  ]
+  const brand = [
+    'Merito.ai','Merito Academy','Helppad'
+  ]
   return (
     <footer className="bg-dark text-white">
       <div className="container py-3">
@@ -9,32 +18,34 @@ function Footer() {
           <div className="col-sm-4 bg-dark">
             <h5 style={{ color: '#ffffff' }}>CONTACT US</h5>
             <ul className="list-unstyled" style={{ color: '#999999' }}>
-              <li>Phone</li>
-              <li>+91 72766-30705</li>
-              <li>Email</li>
-              <li>admin@merito.in</li>
-              <li>Address</li>
-              <li>Baner,Pune,Maharashtra 411045</li>
+              {
+                address.map((el) => {
+                  return <li key={el}>{el}</li>;
+                })
+              }
             </ul>
           </div>
 
           <div className="col-sm-4 bg-dark">
             <h5 style={{ color: '#ffffff' }}>CAREER CORNER</h5>
             <ul className="list-unstyled" style={{ color: '#999999' }}>
-              <li>Our Programs</li>
-              <li>Our Story</li>
-              <li>Our Brands</li>
-              <li>Social Initiatives</li>
-              <li>Contact Us</li>
+  
+              {
+                our.map((el) => {
+                  return <li key={el}>{el}</li>;
+                })
+              }
             </ul>
           </div>
 
           <div className="col-sm-4 bg-dark">
             <h5 style={{ color: '#ffffff' }}>OUR BRANDS</h5>
             <ul className="list-unstyled" style={{ color: '#999999' }}>
-              <li>Merito.ai</li>
-              <li>Merito Academy</li>
-              <li>Helppad</li>
+              {
+                brand.map((el) => {
+                  return <li key={el}>{el}</li>;
+                })
+              }
             </ul>
           </div>
         </div>
