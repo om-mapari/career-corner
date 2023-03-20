589116086058c3e74eb54eda0b664ea902e6201a
om mapari <mapariom05@gmail.com>
2023-03-21 03:37:29 +0530

    Constant added

---
+++ src/Constant.js
@@ -0,0 +1,67 @@
+export const brands = [
+    {
+        brandName: "Merito Academy",
+        mytext:"Our Brand",
+        barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM-2.jpeg?resize=480%2C320&ssl=1"
+    },
+    {
+        brandName: "Merito.in",
+        mytext:"Our Brand",
+        barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM.jpeg?resize=480%2C320&ssl=1"
+    },
+    {
+        brandName: "Merito Academy",
+        mytext:"Our Brand",
+        barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM-2.jpeg?resize=480%2C320&ssl=1"
+    }
+]
+
+export const socialInit = [
+    {
+        brandName: "#Helpped",
+        mytext:"Social Initiatives",
+        barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM-1.jpeg?resize=480%2C320&ssl=1"
+    },
+    {
+        brandName: "#LeadTheChange",
+        mytext:"Social Initiatives",
+        barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.18.04-PM.jpeg?resize=480%2C320&ssl=1"
+    },
+    {
+        brandName: "#WeFightCC",
+        mytext:"Social Initiatives",
+        barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-5.41.14-PM.jpeg?resize=480%2C320&ssl=1"
+    },
+    // {
+    //     brandName: "#Elight",
+    //     mytext:"Social Initiatives",
+    //     barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-7.23.13-PM-1.jpeg?resize=480%2C320&ssl=1"
+    // }
+]
+
+export const TestimonialPerson = [
+    {
+        personName : "Dr Kiran Bedi",
+        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/kb.jpg?resize=100%2C100&ssl=1",
+        personPost : "IPS GOVERNOR OF PUDUCHERRY",
+        personTestimonial : "“Every student is special and possesses unique potential which when harnessed at right time can make miracles. I am glad hat Career Corner is helping students with their guidance program"
+    },
+    {
+        personName : "Dr A.D Sahasrabudhe",
+        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/sb.png?resize=100%2C100&ssl=1",
+        personPost : "CHAIRMAN, AICTE",
+        personTestimonial : "“Knowing the right career is the first step towards building your careers. Career Corner’s programs are addressing this very important need."
+    },
+    {
+        personName : "Shri Arvind Kejriwal",
+        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/ak.jpg?resize=100%2C100&ssl=1",
+        personPost : "CM NEW DELHI",
+        personTestimonial : "“Career guidance is very important for the kids and parents to know which area they can build their careers. I am very happy to see the way these programs are helping students ”"
+    },
+    {
+        personName : "Prof Arvind Sahay",
+        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/arvind-sahay.png?resize=100%2C100&ssl=1",
+        personPost : "DEAN IIM AHMEDABAD",
+        personTestimonial : "“I can see most of the are making default career choices like engineering and MBA however most of them need to ask what is  best career suitable career for me and choose the same”"
+    }
+]
