0f6ff42ddc4d6d363d901a962bc109b29c6250e4
om mapari <mapariom05@gmail.com>
2023-03-20 21:01:07 +0530

    text added

--- src/components/OurPrograms.js
+++ src/components/OurPrograms.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
 
 function OurPrograms() {
   return (
-    <div>OurPrograms</div>
+    <div>
+      <Container className="text-center my-5">
+        <h1>
+          <span className='fs-1' style={{ fontWeight: 'bold', color: 'black' }}>
+            Career choices are LIFE choices
+            We help you to make them RIGHT          </span>
+        </h1>
+        <p className="fs-6 lead text-secondary ">
+          Career Corner is a career services platform founded by COEP and IIM Ahmedabad alumnus with the goal of equipping students and professionals for their career development. Since its inception in 2011 as DISHA, Career Corner has assisted over 50,000 students and professionals in choosing the RIGHT career for them.
+          <br /> <br />We have launched career-oriented programs alongside social initiatives geared towards helping people gain professional excellence.
+        </p>
+        <hr class="bg-dark" />
+
+
+      </Container>
+    </div>
   )
 }
 
