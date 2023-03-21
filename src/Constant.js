9a391a57071960440ef442552357209d345c9247
om mapari <mapariom05@gmail.com>
2023-03-21 10:15:35 +0530

    Constant updated

--- src/Constant.js
+++ src/Constant.js
@@ -1,17 +1,17 @@
 export const brands = [
     {
         brandName: "Merito Academy",
-        mytext:"Our Brand",
+        mytext: "Our Brand",
         barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM-2.jpeg?resize=480%2C320&ssl=1"
     },
     {
         brandName: "Merito.in",
-        mytext:"Our Brand",
+        mytext: "Our Brand",
         barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM.jpeg?resize=480%2C320&ssl=1"
     },
     {
         brandName: "Merito Academy",
-        mytext:"Our Brand",
+        mytext: "Our Brand",
         barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM-2.jpeg?resize=480%2C320&ssl=1"
     }
 ]
@@ -19,17 +19,17 @@ export const brands = [
 export const socialInit = [
     {
         brandName: "#Helpped",
-        mytext:"Social Initiatives",
+        mytext: "Social Initiatives",
         barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.01.04-PM-1.jpeg?resize=480%2C320&ssl=1"
     },
     {
         brandName: "#LeadTheChange",
-        mytext:"Social Initiatives",
+        mytext: "Social Initiatives",
         barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-3.18.04-PM.jpeg?resize=480%2C320&ssl=1"
     },
     {
         brandName: "#WeFightCC",
-        mytext:"Social Initiatives",
+        mytext: "Social Initiatives",
         barndUrl: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/WhatsApp-Image-2022-06-16-at-5.41.14-PM.jpeg?resize=480%2C320&ssl=1"
     },
     // {
@@ -41,64 +41,87 @@ export const socialInit = [
 
 export const TestimonialPerson = [
     {
-        personName : "Dr Kiran Bedi",
-        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/kb.jpg?resize=100%2C100&ssl=1",
-        personPost : "IPS GOVERNOR OF PUDUCHERRY",
-        personTestimonial : "“Every student is special and possesses unique potential which when harnessed at right time can make miracles. I am glad hat Career Corner is helping students with their guidance program"
+        personName: "Dr Kiran Bedi",
+        personImage: "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/kb.jpg?resize=100%2C100&ssl=1",
+        personPost: "IPS GOVERNOR OF PUDUCHERRY",
+        personTestimonial: "“Every student is special and possesses unique potential which when harnessed at right time can make miracles. I am glad hat Career Corner is helping students with their guidance program"
     },
     {
-        personName : "Dr A.D Sahasrabudhe",
-        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/sb.png?resize=100%2C100&ssl=1",
-        personPost : "CHAIRMAN, AICTE",
-        personTestimonial : "“Knowing the right career is the first step towards building your careers. Career Corner’s programs are addressing this very important need."
+        personName: "Dr A.D Sahasrabudhe",
+        personImage: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/sb.png?resize=100%2C100&ssl=1",
+        personPost: "CHAIRMAN, AICTE",
+        personTestimonial: "“Knowing the right career is the first step towards building your careers. Career Corner’s programs are addressing this very important need."
     },
     {
-        personName : "Shri Arvind Kejriwal",
-        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/ak.jpg?resize=100%2C100&ssl=1",
-        personPost : "CM NEW DELHI",
-        personTestimonial : "“Career guidance is very important for the kids and parents to know which area they can build their careers. I am very happy to see the way these programs are helping students ”"
+        personName: "Shri Arvind Kejriwal",
+        personImage: "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/ak.jpg?resize=100%2C100&ssl=1",
+        personPost: "CM NEW DELHI",
+        personTestimonial: "“Career guidance is very important for the kids and parents to know which area they can build their careers. I am very happy to see the way these programs are helping students ”"
     },
     {
-        personName : "Prof Arvind Sahay",
-        personImage : "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/arvind-sahay.png?resize=100%2C100&ssl=1",
-        personPost : "DEAN IIM AHMEDABAD",
-        personTestimonial : "“I can see most of the are making default career choices like engineering and MBA however most of them need to ask what is  best career suitable career for me and choose the same”"
+        personName: "Prof Arvind Sahay",
+        personImage: "https://i0.wp.com/career-corner.in/wp-content/uploads/2015/11/arvind-sahay.png?resize=100%2C100&ssl=1",
+        personPost: "DEAN IIM AHMEDABAD",
+        personTestimonial: "“I can see most of the are making default career choices like engineering and MBA however most of them need to ask what is  best career suitable career for me and choose the same”"
     }
 ]
 
 
 export const courseData = [
     {
-      courseName: 'Career Assessment Test',
-      courseDetails: `Our career assessment tests help students to discover their aptitude as well as personality. The test is conducted online and report is shared over the email. 
+        courseName: 'Career Assessment Test',
+        courseDetails: `Our career assessment tests help students to discover their aptitude as well as personality. The test is conducted online and report is shared over the email. 
 
       The test report also covers matching study streams and careers based on students ability, interest and personality. `,
-      courseFees: 1000,
-      imageUrl: 'https://i0.wp.com/career-corner.in/wp-content/uploads/2022/06/Test.webp?resize=1024%2C768&ssl=1'
+        courseFees: 1000,
+        imageUrl: 'https://i0.wp.com/career-corner.in/wp-content/uploads/2022/06/Test.webp?resize=1024%2C768&ssl=1'
     },
     {
-      courseName: 'Career Assessment Test & Counselling',
-      courseDetails: `This program consists of career assessment test followed by counselling session by our expert counsellors. Bot the test as well as counselling session would be online. 
+        courseName: 'Career Assessment Test & Counselling',
+        courseDetails: `This program consists of career assessment test followed by counselling session by our expert counsellors. Bot the test as well as counselling session would be online. 
 
       The test helps student to discover their aptitude and personality. The counselling session helps to identify RIGHT career for them. `,
-      courseFees: 2000,
-      imageUrl: 'https://i0.wp.com/career-corner.in/wp-content/uploads/2022/06/Guidance.webp?resize=1024%2C768&ssl=1'
+        courseFees: 2000,
+        imageUrl: 'https://i0.wp.com/career-corner.in/wp-content/uploads/2022/06/Guidance.webp?resize=1024%2C768&ssl=1'
     },
     // Add more courses as needed...
-  ];
+];
 
 
-  export const OurStoryData = [
+export const OurStoryData = [
     {
-      PointHeading: 'Our Vision',
-      Point: 'Our goal is to become Indias foremost career services organization, providing students and professionals with comprehensive career services'
+        PointHeading: 'Our Vision',
+        Point: 'Our goal is to become Indias foremost career services organization, providing students and professionals with comprehensive career services'
     },
     {
-      PointHeading: 'Our Mission Statement',
-      Point: 'To democratise career education for the public by collaborating with individuals, educational institutes, and Non-Government Organizations'
+        PointHeading: 'Our Mission Statement',
+        Point: 'To democratise career education for the public by collaborating with individuals, educational institutes, and Non-Government Organizations'
     },
     {
-      PointHeading: 'Our Belief System',
-      Point: 'We are a team of dedicated individuals who were fortunate to get education from India’s premium engineering and management colleges. We felt indebted to schools we attended and guidance we received from our teachers which made our career journey possible. Career Corner is nothing but our combined effort to give back what we have received!'
+        PointHeading: 'Our Belief System',
+        Point: 'We are a team of dedicated individuals who were fortunate to get education from India’s premium engineering and management colleges. We felt indebted to schools we attended and guidance we received from our teachers which made our career journey possible. Career Corner is nothing but our combined effort to give back what we have received!'
     }
-  ]
+]
+
+
+export const FocusAreaData = [
+    {
+        Image: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/ed.jpg?resize=600%2C338&ssl=1",
+        Information: `Our education initiatives aimed at helping students to identify and get into the RIGHT career. We are providing career discovery programs for students and professionals through Merito Academy. Merito Academy will also provide training and certification for meeting the industry need of hiring jobs-ready candidates across different entry level job roles.
+
+      Through our social initiatives like Enlight, #LeadtheChange and #WeFightCC wehave helped more than 50,000 students to make RIGHT career decisions.`
+    },
+    {
+        Image: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/em.jpg?resize=600%2C334&ssl=1",
+        Information: `
+      Our employment initiatives are aimed at helping students & professionals for getting RIGHT jobs and companies to hire quality talent. Merito which is our curated talent platform aims at helping growth companies to hire handpicked candidates for job roles across levels. Merito is pioneer in assessment driven hiring and changing the way hiring is done.
+      
+      At present we are working with more than 20 clients and hiring 100+ open positions across different levels and job roles.`
+    },
+    {
+        Image: "https://i0.wp.com/career-corner.in/wp-content/uploads/2022/05/entrepreneurship-tycoon-small-business-enterprise-concept-83519529.jpg?resize=600%2C415&ssl=1",
+        Information: `Our entrepreneurship initiatives are aimed at helping students and professionals for their entrepreneurship development. Our entrepreneurship development program - SEED, helps individual from idea to execution stages of their start-up journey. We collaborate with educational institutes to provide these programs.
+
+      We are mentoring early stage start-ups for maneuvering challenges in their start-up journey by leveraging our experience.`
+    }
+]
